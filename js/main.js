c9daee1fd25160053014e191691364e05e02d9f0
Marta-ms <Marta-ms>
2024-11-07 11:35:59 +0100

    fetch

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
 "use strict"
 
+/*Pintar tre imágenes de portadas de series anime
+ -Conseguir la información -- > peticion al servidor para obtener la 3 imágenes 
+
+*/
+
+fetch("https://api.jikan.moe/v4/anime?q=naruto")
+.then((response) => {
+    return response.json();
+})
+.then((data) => {
+    console.log(data)
+    const images = data.images;
+    console.log(images);
+})
+
+//creo ul vacía
